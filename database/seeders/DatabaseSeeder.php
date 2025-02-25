e6f34cb914e8a23ecbf34c4aa3b73b51bc0c3a64
kuswandi-ti <kuswandi.ti@gmail.com>
2025-02-25 11:42:59 +0700

    25-02-2025 : 6th Commit : Update Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\FormatDateTableSeeder;
+use Database\Seeders\FormatTimeTableSeeder;
 use Database\Seeders\SettingSystemTableSeeder;
 use Database\Seeders\RolePermissionTableSeeder;
+use KodePandai\Indonesia\IndonesiaDatabaseSeeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +17,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(RolePermissionTableSeeder::class);
-        $this->call(SettingSystemTableSeeder::class);
+        // $this->call(RolePermissionTableSeeder::class);
+        // $this->call(SettingSystemTableSeeder::class);
+        // $this->call(FormatDateTableSeeder::class);
+        // $this->call(FormatTimeTableSeeder::class);
+        // $this->call(IndonesiaDatabaseSeeder::class);
     }
 }
--- public/template_admin/assets/js/masonry.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict'
-    var grid = document.querySelector('.grid');
-    var msnry = new Masonry(grid, {
-        // options...
-        itemSelector: '.grid-item',
-        columnWidth: 0
-    });
-})();
